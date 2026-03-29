f626c830306ebc159ed27b3aafd87069d055fc2c
ngs_liuji <ngs_liuji@qq.com>
2026-03-29 22:34:51 +0800

    测试，前端联调，实现应用ai生成、静态页面部署等功能
    Signed-off-by: ngs_liuji <ngs_liuji@qq.com>

--- src/main/java/com/ngsliuji/ngsaicodemother/controller/StaticResourceController.java
+++ src/main/java/com/ngsliuji/ngsaicodemother/controller/StaticResourceController.java
@@ -21,10 +21,6 @@ public class StaticResourceController {
     // 应用生成根目录（用于浏览）
     private static final String PREVIEW_ROOT_DIR = AppConstant.CODE_OUTPUT_ROOT_DIR;
 
-//TODO
-    /**
-     * 这里多了个{deployKey}，前端没有
-     */
     /**
      * 提供静态资源访问，支持目录重定向
      * 访问格式：http://localhost:8123/api/static/{deployKey}[/{fileName}]
