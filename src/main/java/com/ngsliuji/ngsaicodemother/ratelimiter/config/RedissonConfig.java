34dc844b357081f096d0c92c91f644c73f559afe
ngs_liuji <ngs_liuji@qq.com>
2026-04-02 21:02:44 +0800

    工具调用优化
    提供退出工具
    还可以为 AI 提供一个专门的退出工具，让它能够主动结束工具调用循环。在 ai.tools 包下新建一个继承了 BaseTool 的工具类，这样可以被 ToolManager 自动注册
    
    Signed-off-by: ngs_liuji <ngs_liuji@qq.com>

--- src/main/java/com/ngsliuji/ngsaicodemother/ratelimiter/config/RedissonConfig.java
+++ src/main/java/com/ngsliuji/ngsaicodemother/ratelimiter/config/RedissonConfig.java
@@ -1,10 +1,10 @@
 package com.ngsliuji.ngsaicodemother.ratelimiter.config;
 
-import lombok.Value;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.redisson.config.SingleServerConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
