f26798872b225ab071d8772dd2d873eeea106654
ngs_liuji <ngs_liuji@qq.com>
2026-04-02 16:42:40 +0800

    Redis 缓存优化
    1.检查配置文件中的 Redis 连接信息.
    2.实现缓存 key 生成工具类,既保证了不同查询请求的 key 唯一，又避免了 key 过长的问题.
    3.在 Spring Boot 启动类上添加 @EnableCaching 注解，支持 Spring Data 缓存注解.
    4.必须配置 Redis 缓存管理器 CacheManager，这是 Spring Cache 的核心组件。如果不配置的话，使用缓存注解时可能会报错.
    5.在 AppController 中为精选应用列表接口添加缓存注解.
    
    Signed-off-by: ngs_liuji <ngs_liuji@qq.com>

--- src/main/java/com/ngsliuji/ngsaicodemother/NgsAiCodeMotherApplication.java
+++ src/main/java/com/ngsliuji/ngsaicodemother/NgsAiCodeMotherApplication.java
@@ -4,10 +4,11 @@ import dev.langchain4j.community.store.embedding.redis.spring.RedisEmbeddingStor
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 //在启动类中排除 em؜b؜e؜dding؜ 的؜自动؜装配，因为本项目用不到，禁用的是向量数据库RAG会用到，但是这个项目没有
 @SpringBootApplication(exclude = {RedisEmbeddingStoreAutoConfiguration.class})
-
+@EnableCaching
 @MapperScan("com.ngsliuji.ngsaicodemother.mapper")
 public class NgsAiCodeMotherApplication {
 
